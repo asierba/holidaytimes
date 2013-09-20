80c277cedfc940428bdd183981f8e2e68c224609
Asier Barrenetxea <asier.barrenetxea-arando@wiggle.co.uk>
2013-09-20 15:32:28 +0100

    Booking Controller created with two empty actions and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Bookmeoff::Application.routes.draw do
-  get "welcome/index"
+  get "booking/index"
+  get "booking/request"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'booking#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
