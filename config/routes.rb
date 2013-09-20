e6f5802919685663b76294d2ef76da3d1ffdd4b9
asierba <asierba@gmail.com>
2013-09-20 16:43:20 +0100

    Routes changed to use resources :bookings

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Bookmeoff::Application.routes.draw do
-  get "booking/index"
-  get "booking/request"
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  root 'bookings#index'
 
-  # You can have the root of your site routed with "root"
-  root 'booking#index'
+  resources :bookings
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -13,8 +9,7 @@ Bookmeoff::Application.routes.draw do
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+
 
   # Example resource route with options:
   #   resources :products do
