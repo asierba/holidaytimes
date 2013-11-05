f777e0f5abcfe12c4f80c820560f1e9de9f0c8bd
henry.keen <henry.keen@wiggle.co.uk>
2013-11-05 18:09:12 +0000

    Fail silently if ldap config does not exist

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,19 +2,25 @@ OmniAuth.config.logger = Rails.logger
 
 HasAuthProvider = false
 
-LDAPConfig = YAML.load(File.open("#{Rails.root}/config/ldap.yml"))
+@LdapConfigPath = "#{Rails.root}/config/ldap.yml"
 
-if LDAPConfig != nil && LDAPConfig['ldap']['enabled']
-  Bookmeoff::Application.config.middleware.use OmniAuth::Builder do
-    provider :ldap,
-             :host => LDAPConfig['ldap']['host'],
-             :port => LDAPConfig['ldap']['port'],
-             :method => LDAPConfig['ldap']['method'],
-             :base => LDAPConfig['ldap']['base'],
-             :uid => LDAPConfig['ldap']['uid'],
-             :bind_dn => LDAPConfig['ldap']['bind_dn'],
-             :password => LDAPConfig['ldap']['password']
+if File.exists?(@LdapConfigPath)
+
+  LDAPConfig = YAML.load(File.open(@LdapConfigPath))
+
+  if LDAPConfig != nil && LDAPConfig['ldap']['enabled']
+    Bookmeoff::Application.config.middleware.use OmniAuth::Builder do
+      provider :ldap,
+               :host => LDAPConfig['ldap']['host'],
+               :port => LDAPConfig['ldap']['port'],
+               :method => LDAPConfig['ldap']['method'],
+               :base => LDAPConfig['ldap']['base'],
+               :uid => LDAPConfig['ldap']['uid'],
+               :bind_dn => LDAPConfig['ldap']['bind_dn'],
+               :password => LDAPConfig['ldap']['password']
+    end
+
+    HasAuthProvider = true
   end
 
-  HasAuthProvider = true
 end
