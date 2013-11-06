72e360dcb49e97471fe917c3e1783574886b3c64
henry.keen <henry.keen@wiggle.co.uk>
2013-11-06 17:28:08 +0000

    Make the from and to date required

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,9 +1,12 @@
 class Booking < ActiveRecord::Base
   validates :who, presence: true,length: { minimum: 5 }
+  validates :from, presence: true
+  validates :to, presence: true
+
   validate :from_date_must_be_earlier_than_to_date
 
   def from_date_must_be_earlier_than_to_date
-    errors.add(:from, 'date must be earlier than to date') if
-        from > to
+    errors.add(:from, 'must be earlier than to') if
+        from != nil && to != nil && from > to
   end
 end
