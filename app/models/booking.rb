43e8f7c95f35148414f4e52811bfb9ed462fbb34
henry.keen <henry.keen@wiggle.co.uk>
2013-11-06 17:24:26 +0000

    fix bug where from date can be after the to date

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,9 @@
 class Booking < ActiveRecord::Base
-  validates :who, presence: true, length: { minimum: 5 }
+  validates :who, presence: true,length: { minimum: 5 }
+  validate :from_date_must_be_earlier_than_to_date
+
+  def from_date_must_be_earlier_than_to_date
+    errors.add(:from, 'date must be earlier than to date') if
+        from > to
+  end
 end
